a80d3398b6edec5749fa71fdf1175ec65fdb655c
mauli karche <maulikarche36@gmail.com>
2024-01-17 13:24:09 +0530

    commit8

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-function offbulb()
-{
-    const element = document.getElementById("onoff");
-  element.src= "";
+
+function showSlides() {
+  let i;
+}
+
+if (window.innerWidth>=490) {
+  document.getElementById('showMenu').style.display="none"
+  document.getElementById('hideMenu').style.display="none"
+}
+function showMenu() {
+  document.getElementById('menuItems').style.left="0px"
+}
+function hideMenu() {
+  document.getElementById('menuItems').style.left="-300px"
 }
